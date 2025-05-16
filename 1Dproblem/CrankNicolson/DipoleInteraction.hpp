ebb459e40b719372dd03bb333fd1afe181de9b20
Yahor Mankevich <mankeviczjegor@gmail.com>
2025-05-16 15:50:53 +0200

    added ddi 1D fftw

--- 1Dproblem/CrankNicolson/DipoleInteraction.hpp
+++ 1Dproblem/CrankNicolson/DipoleInteraction.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include "definitions.hpp"
 
 // // Complementary error function approximation (from Numerical Recipes)
 // double erfc_approx(double x) {
@@ -15,7 +16,11 @@
 //     return x >= 0.0 ? ans : 2.0 - ans;
 // }
 
-class DipolarInteraction2D {
+template <Dimension Dim>
+class DipolarInteraction;
+
+template <>
+class DipolarInteraction<Dimension::Two> {
 private:
     int Nx, Ny;
     double Lx, Ly;
@@ -34,7 +39,7 @@ private:
     Eigen::MatrixXd U_tilde;
     
 public:
-    DipolarInteraction2D(int nx, int ny, double lx, double ly, double confinement_length, double interaction_strength)
+    DipolarInteraction(int nx, int ny, double lx, double ly, double confinement_length, double interaction_strength)
         : Nx(nx), Ny(ny), Lx(lx), Ly(ly), lz(confinement_length), Cdd(interaction_strength) {
         
         dx = Lx / Nx;
@@ -54,7 +59,7 @@ public:
         precompute_U_tilde();
     }
     
-    ~DipolarInteraction2D() {
+    ~DipolarInteraction() {
         fftw_destroy_plan(plan_forward);
         fftw_destroy_plan(plan_backward);
         fftw_free(density_real);
@@ -117,3 +122,188 @@ public:
         }
     }
 };
+
+
+
+template<>
+class DipolarInteraction<Dimension::One> {
+private:
+    int N;
+    double L;
+    double lt;
+    double Cdd;
+    double dx;
+    
+    // FFTW plans and arrays
+    fftw_plan plan_forward, plan_backward;
+    double* density_real;
+    fftw_complex* density_fourier;
+    fftw_complex* potential_fourier;
+    double* potential_real;
+    
+    // Precomputed U_tilde
+    Eigen::VectorXd U_tilde;
+public: 
+    DipolarInteraction(int n, double l, double confinement_length, double interaction_strength)
+            : N(n), L(l), lt(confinement_length), Cdd(interaction_strength) {
+            
+        dx = L / N;
+        
+        // Allocate memory for FFTW
+        density_real = (double*)fftw_malloc(sizeof(double) * N);
+        density_fourier = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (N/2 + 1));
+        potential_fourier = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (N/2+ 1));
+        potential_real = (double*)fftw_malloc(sizeof(double) * N);
+        
+        // Create FFTW plans
+        plan_forward = fftw_plan_dft_r2c_1d(N, density_real, density_fourier, FFTW_MEASURE);
+        plan_backward = fftw_plan_dft_c2r_1d(N,potential_fourier, potential_real, FFTW_MEASURE);
+        
+        // Precompute U_tilde
+        precompute_U_tilde();
+    }
+
+    ~DipolarInteraction() {
+        fftw_destroy_plan(plan_forward);
+        fftw_destroy_plan(plan_backward);
+        fftw_free(density_real);
+        fftw_free(density_fourier);
+        fftw_free(potential_fourier);
+        fftw_free(potential_real);
+    }
+private:
+    void precompute_U_tilde(){
+        U_tilde.resize(N/2 + 1);
+        for (int k = 0; k <= N/2; ++k) {
+            double kx = 2.0 * M_PI * k / L;
+            double kappa = std::abs(kx) * lt / std::sqrt(2.0);
+
+            if (kx == 0.0) {
+                U_tilde[k] = 2.0 * Cdd / 3.0;
+            } else {
+                U_tilde[k] = (Cdd / 3.0) * (2.0 - 3.0 * M_PI * kappa * std::exp(kappa * kappa) * std::erfc(kappa));
+            }
+        }
+    }
+public:
+    void compute_DDI_term(const Eigen::VectorXcd& psi, Eigen::VectorXd& Phi_DDI) {
+        // Fill density array
+        for (int i = 0; i < N; ++i) {
+            density_real[i] = std::norm(psi(i));
+        }
+
+        // FFT
+        fftw_execute(plan_forward);
+
+        // Multiply by kernel in Fourier space
+        for (int k = 0; k <= N/2; ++k) {
+            potential_fourier[k][0] = U_tilde[k] * density_fourier[k][0]; // Real
+            potential_fourier[k][1] = U_tilde[k] * density_fourier[k][1]; // Imag
+        }
+
+        // Inverse FFT
+        fftw_execute(plan_backward);
+
+        // Normalize and copy result
+        Phi_DDI.resize(N);
+        for (int i = 0; i < N; ++i) {
+            Phi_DDI[i] = potential_real[i] / N;
+        }
+    }
+};
+
+
+/*
+#pragma once
+
+#include <Eigen/Dense>
+#include <fftw3.h>
+#include <cmath>
+#include <complex>
+
+class DipolarInteraction1D {
+private:
+    int N;
+    double L;
+    double dx;
+    double lz;
+    double Cdd;
+
+    fftw_plan plan_forward, plan_backward;
+    double* density_real;
+    fftw_complex* density_fourier;
+    fftw_complex* potential_fourier;
+    double* potential_real;
+
+    Eigen::VectorXd U_tilde; // kernel in Fourier space
+
+public:
+    DipolarInteraction1D(int n, double length, double confinement_length, double interaction_strength)
+        : N(n), L(length), lz(confinement_length), Cdd(interaction_strength) {
+
+        dx = L / N;
+
+        // FFTW memory allocation
+        density_real = (double*)fftw_malloc(sizeof(double) * N);
+        density_fourier = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (N/2 + 1));
+        potential_fourier = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (N/2 + 1));
+        potential_real = (double*)fftw_malloc(sizeof(double) * N);
+
+        // FFTW plans
+        plan_forward = fftw_plan_dft_r2c_1d(N, density_real, density_fourier, FFTW_MEASURE);
+        plan_backward = fftw_plan_dft_c2r_1d(N, potential_fourier, potential_real, FFTW_MEASURE);
+
+        precompute_U_tilde();
+    }
+
+    ~DipolarInteraction1D() {
+        fftw_destroy_plan(plan_forward);
+        fftw_destroy_plan(plan_backward);
+        fftw_free(density_real);
+        fftw_free(density_fourier);
+        fftw_free(potential_fourier);
+        fftw_free(potential_real);
+    }
+
+private:
+    void precompute_U_tilde() {
+        U_tilde.resize(N/2 + 1);
+        for (int k = 0; k <= N/2; ++k) {
+            double kx = 2.0 * M_PI * k / L;
+            double kappa = std::abs(kx) * lz / std::sqrt(2.0);
+
+            if (kx == 0.0) {
+                U_tilde[k] = 2.0 * Cdd / 3.0;
+            } else {
+                U_tilde[k] = (Cdd / 3.0) * (2.0 - 3.0 * M_PI * kappa * std::exp(kappa * kappa) * std::erfc(kappa));
+            }
+        }
+    }
+
+public:
+    void compute_DDI_term(const Eigen::VectorXcd& psi, Eigen::VectorXd& Phi_DDI) {
+        // Fill density array
+        for (int i = 0; i < N; ++i) {
+            density_real[i] = std::norm(psi(i));
+        }
+
+        // FFT
+        fftw_execute(plan_forward);
+
+        // Multiply by kernel in Fourier space
+        for (int k = 0; k <= N/2; ++k) {
+            potential_fourier[k][0] = U_tilde[k] * density_fourier[k][0]; // Real
+            potential_fourier[k][1] = U_tilde[k] * density_fourier[k][1]; // Imag
+        }
+
+        // Inverse FFT
+        fftw_execute(plan_backward);
+
+        // Normalize and copy result
+        Phi_DDI.resize(N);
+        for (int i = 0; i < N; ++i) {
+            Phi_DDI[i] = potential_real[i] / N;
+        }
+    }
+};
+*/
